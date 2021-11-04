1b74e9d62d3579a02b3f6dfc6d610cf62b9d23ba
OrranaLhaynher <orranal396@gmail.com>
2021-11-04 17:53:52 -0300

    Outras rotas e json

--- Tutorial 01/app.py
+++ Tutorial 01/app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello():
     return "Olá mundo maravilhoso!"
 
+@app.route('/hithere', methods=['GET'])
+def hithere():
+    return "Olá, eu pressionei /hithere."
+
+@app.route('/bye', methods=['GET'])
+def bye():
+    json_test = {
+        'key' : 0,
+        'text' : 'Testando retorno com json.'
+    }
+    return jsonify(json_test)
+
 if __name__ == '__main__' :
-    app.run()
+    app.run(debug=True) #debug=True mostra erros ao rodar server
